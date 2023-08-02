381a43d10b2930c392153d81c078fcd1b8960398
Tamás Russ <tamas.russ@tuta.io>
2023-08-02 12:19:25 +0200

    Updates

--- _app/immutable/chunks/plugin_.d1fb2379.js
+++
@@ -1,1 +0,0 @@
-import{d as e,w as a}from"./index.4457f1a3.js";const o=[{name:"_Employees",path:"employees",src:"http://localhost:52952/employees.js"},{name:"_Admins",path:"admins",src:"http://localhost:53173/leads.js"}],t=new Map;o.forEach(s=>t.set(s.path,s));const l=t,n=a(l),c=e(n,s=>s);export{c as p};
